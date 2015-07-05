1e770270197cc4bd716e4a0742ec714ff3783b14
Russell Ballestrini <russell.ballestrini@mandiant.com>
2015-07-05 18:03:35 -0400

    Test EnrichedInstance equality.
    	modified:   tests/unit/test_enriched_instance.py

--- tests/unit/test_enriched_instance.py
+++ tests/unit/test_enriched_instance.py
@@ -6,6 +6,7 @@ from botoform.evpc import EnrichedInstance
 
 class MockInstanceSpec(object):
     """Mock Boto3's ec2.Instance class."""
+    id   = 'i-mock1234'
     tags = [{u'Value': 'vpc-mock1234', u'Key': 'vpc-id'},
             {u'Value': 'webapp01-web01', u'Key': 'Name'}]
 
@@ -13,13 +14,21 @@ class TestEnrichedInstance(TestCase):
 
     def setUp(self):
         MockInstance = Mock(name="Instance", return_value = MockInstanceSpec())
-        self.einstance = EnrichedInstance(MockInstance())
+        self.einstance1 = EnrichedInstance(MockInstance())
+        self.einstance2 = EnrichedInstance(MockInstance())
+
+    def test_equal(self):
+        self.assertEqual(self.einstance1, self.einstance2)
+
+    def test_not_equal(self):
+        self.einstance2.id = 'i-mock4321'
+        self.assertNotEqual(self.einstance1, self.einstance2)
 
     def test_hostname(self):
-        self.assertEqual('webapp01-web01', self.einstance.hostname)
+        self.assertEqual('webapp01-web01', self.einstance1.hostname)
 
     def test_shortname(self):
-        self.assertEqual('web01', self.einstance.shortname)
+        self.assertEqual('web01', self.einstance1.shortname)
 
     def test_role(self):
-        self.assertEqual('web', self.einstance.role)
+        self.assertEqual('web', self.einstance1.role)

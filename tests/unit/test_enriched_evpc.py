dc73ba4936e015ec320d1975f8e968856b03dd64
Russell Ballestrini <russell.ballestrini@mandiant.com>
2015-07-05 21:21:40 -0400

    use MagicMock instead of lambda
    	modified:   tests/unit/test_enriched_evpc.py

--- tests/unit/test_enriched_evpc.py
+++ tests/unit/test_enriched_evpc.py
@@ -1,21 +1,23 @@
 from helpers import BotoformTestCase
 
+from mock import MagicMock
+
 class TestEnrichedVPC(BotoformTestCase):
 
     def test_get_vpc_by_name_tag(self):
         # mock the _get_vpcs_by_filter method to return a list with one vpc.
-        self.evpc1._get_vpcs_by_filter = lambda x : [1]
+        self.evpc1._get_vpcs_by_filter = MagicMock(return_value=[1])
         self.evpc1.get_vpc_by_name_tag('webapp01')
 
     def test_get_vpc_by_name_tag_no_vpc_error(self):
         # mock the _get_vpcs_by_filter method to return an empty list.
-        self.evpc1._get_vpcs_by_filter = lambda x : []
+        self.evpc1._get_vpcs_by_filter = MagicMock(return_value=[])
         with self.assertRaises(Exception):
             self.evpc1.get_vpc_by_name_tag('webapple01')
 
     def test_get_vpc_by_name_tag_multi_vpcs_error(self):
         # mock the _get_vpcs_by_filter method to return a list with two vpcs.
-        self.evpc1._get_vpcs_by_filter = lambda x : [1, 2]
+        self.evpc1._get_vpcs_by_filter = MagicMock(return_value=[1, 2])
         with self.assertRaises(Exception):
             self.evpc1.get_vpc_by_name_tag('webapp*')
 

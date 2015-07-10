84a8e1780ff0e74446d5f15ef1c6fbe15234825f
Russell Ballestrini <russell.ballestrini@gmail.com>
2015-07-09 22:11:43 -0400

    Update dump.py
    spelling typo

--- botoform/plugins/dump.py
+++ botoform/plugins/dump.py
@@ -56,7 +56,7 @@ class SecurityGroups(object):
 
     @staticmethod
     def main(args, evpc):
-        """Output Security Groups in a Botoform compatable format."""
+        """Output Security Groups in a Botoform compatible format."""
         sgs = list(evpc.security_groups.all())
         sgs = {}
         for sg in evpc.security_groups.all():

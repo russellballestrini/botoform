ffbc900250cb55ad31fb7a8e183b84d76b1b49c6
Russell Ballestrini <russell.ballestrini@mandiant.com>
2015-10-26 11:20:59 -0400

    Pulling subnetallocator from husky project.
    I performed some minor refactors on the allocate function.
    
    	new file:   botoform/subnetallocator.py
    	new file:   tests/unit/test_subnetallocator.py

---
+++ botoform/subnetallocator.py
@@ -0,0 +1,43 @@
+import netaddr
+
+def allocate(cidrs, sizes):
+    """
+    Accept network block CIDRs and list of subnetwork CIDR sizes.
+    Return a list of IPNetwork objects allocated from the network block.
+    """
+
+    if isinstance(cidrs, str):
+        cidrs = [netaddr.IPNetwork(cidrs)]
+
+    if len(sizes) == 0:
+        return []
+
+    sizes = sorted(sizes)
+    need_size = sizes[0]
+    need_count = sizes.count(need_size)
+    sizes = sizes[need_count:]
+
+    allocated_cidrs = []
+    remaining_cidrs = []
+
+    for c in cidrs:
+        if need_count == 0:
+            remaining_cidrs.append(c)
+            continue
+
+        subnets = list(c.subnet(need_size))
+
+        if need_count > len(subnets):
+            allocated_cidrs += subnets
+            need_count -= len(subnets)
+        else:
+            allocated_cidrs += subnets[0:need_count]
+            remaining_cidrs += subnets[need_count:]
+            need_count = 0
+
+    if need_count != 0:
+        raise Exception("Not enough room! Need %d more subnets of size %d but can't get any" % (need_count, need_size))
+
+    # Split off remainder and allocate that
+    allocated_cidrs += allocate(remaining_cidrs, sizes)
+    return allocated_cidrs
---
+++ tests/unit/test_subnetallocator.py
@@ -0,0 +1,42 @@
+from unittest import TestCase
+
+from husky.subnetallocator import allocate
+
+class TestSubnetAllocator(TestCase):
+
+    def test_allocate_returns_desired_network_list(self):
+        desired_allocations = ['10.0.6.0/26', '10.0.6.64/29', '10.0.6.72/29']
+        allocations = map(str, allocate('10.0.6.0/24', [26,29,29]))
+        self.assertListEqual(allocations, desired_allocations)
+        self.assertEqual(len(allocations), 3)
+
+    def test_allocate_sizes_sorted(self):
+        desired_allocations = ['10.0.6.0/26', '10.0.6.64/29', '10.0.6.72/29']
+        allocations = map(str, allocate('10.0.6.0/24', [29,26,29]))
+        self.assertListEqual(allocations, desired_allocations)
+        self.assertEqual(len(allocations), 3)
+
+    def test_allocate_many_subnets(self):
+        desired_allocations = ['10.0.6.0/26', '10.0.6.64/26',
+                               '10.0.6.128/26', '10.0.6.192/27',
+                               '10.0.6.224/28', '10.0.6.240/28']
+        allocations = map(str, allocate('10.0.6.0/24', [26,26,26,27,28,28]))
+        self.assertEqual(len(allocations), 6)
+        self.assertListEqual(allocations, desired_allocations)
+
+    def test_allocate_many_subnets_2(self):
+        desired_allocations = ['10.0.6.0/26', '10.0.6.64/26', '10.0.6.128/27',
+                               '10.0.6.160/27', '10.0.6.192/27', '10.0.6.224/28']
+        allocations = map(str, allocate('10.0.6.0/24', [26,26,27,27,27,28]))
+        self.assertEqual(len(allocations), 6)
+        self.assertListEqual(allocations, desired_allocations)
+
+    def test_allocate_too_many_subnets_exception(self):
+        with self.assertRaises(Exception):
+            allocate('10.0.6.0/24', [26,26,26,27,28,28,29])
+
+    def test_allocate_too_many_subnets_exception_2(self):
+        with self.assertRaises(Exception):
+            allocate('10.0.6.0/24', [26,26,26,27,28,28,28])
+
+

318e0fb10f0635d7b3507b04ba6352fe20b9136a
Russell Ballestrini <russell.ballestrini@mandiant.com>
2015-11-03 20:38:20 -0500

    modified:   botoform/config.py 	modified:   docs/source/examples/webapp.yaml 	renamed:    tests/amis.yaml -> tests/fixtures/nested/amis.yaml 	renamed:    tests/webapp.yaml -> tests/fixtures/webapp.yaml 	modified:   tests/unit/test_config_loader.py

--- botoform/config.py
+++ botoform/config.py
@@ -1,3 +1,4 @@
+from os import path
 import yaml
 from jinja2 import (
   Environment,
@@ -7,63 +8,52 @@ from jinja2 import (
 
 #from util import get_port_range
 
-class ConfigLoader:
-    def __init__(self, template_dir="templates", context_vars=None):
-        self.jinja2_env = Environment(loader=FileSystemLoader(template_dir))
+class ConfigLoader(object):
+    def __init__(self, context_vars=None, template_dir=None):
+        self.jinja2_env = None
+        self._template_dir = None
         self.context_vars = context_vars if context_vars is not None else {}
+        if template_dir is not None:
+            self.template_dir = template_dir
+
+    @property
+    def template_dir(self):
+        return self._template_dir
+
+    @template_dir.setter
+    def template_dir(self, new_template_dir):
+        self.jinja2_env = Environment(loader=FileSystemLoader(new_template_dir))
+        self._template_dir = new_template_dir
 
     def _render_template(self, template):
         return template.render(self.context_vars)
 
     def render(self, template_file):
-        """Return Jinja2 render of template file with context_vars."""
+        """Return Jinja2 render of template with context_vars."""
         return self._render_template(self.jinja2_env.get_template(template_file))
 
     def render_string(self, template_string):
         """Return Jinja2 render of template_str with context_vars."""
         return self._render_template(Template(template_string))
 
-    def _load(self, template_file=None, template_string=None):
-        if template_file is None and template_string is None:
-            raise Exception('missing template template_file or template_string')
+    def _load(self, template_path=None, template_string=None):
+        if template_path is None and template_string is None:
+            raise Exception('missing template template_path or template_string')
         if template_string:
             rendered = self.render_string(template_string)
-        if template_file:
+        if template_path:
+            if self.jinja2_env is None:
+                self.template_dir = path.dirname(template_path)
+                template_file = path.basename(template_path)
+            else:
+                template_file = template_path
             rendered = self.render(template_file)
         return yaml.load(rendered)
 
     def _load_includes(self, config):
         """Load YAML path includes and return config. Will clobber existing."""
-        for key, path in config['inlcudes'].items():
-            config[key] = self._load(template_file=path)[key]
-        return config
-
-    def _remove_excluded_roles(self, config, exclude_roles):
-        """remove excluded roles and security groups from config"""
-        exclude_roles = exclude_roles if exclude_roles is not None else []
-        for role in exclude_roles:
-
-            # remove excluded sg references.
-            for key, value in config['instance_roles'].items():
-                if role in value['security_groups']:
-                    config['instances'][key]['security_groups'].remove(role)
-
-            # remove excluded instance roles.
-            if role in config.get('instance_roles', {}):
-                del config['instance_roles'][role]
-
-            # remove excluded security_groups.
-            if role in config.get('security_groups', {}):
-                del config['security_groups'][role]
-
-            # remove excluded security_group rule references.
-            for sg_name, sg_rules in config['security_groups'].items():
-                clean_rules = []
-                for rule in sg_rules:
-                    if rule[0] != role:
-                        clean_rules.append(rule)
-                config['security_groups'][sg_name] = clean_rules
-
+        for key, path in config['includes'].items():
+            config[key] = self._load(template_path=path)[key]
         return config
 
     def _mutate_port_ranges(self, config):
@@ -75,13 +65,12 @@ class ConfigLoader:
                 rule[2] = get_port_range(rule[2], rule[1])
         return config
 
-    def load(self, template_file=None, template_string=None, exclude_roles=None):
+    def load(self, template_path=None, template_string=None):
         """
         Use Jinja2 to render template with context_vars,
-        return Python representation of JSON config.
+        return Python representation of YAML config.
         """
-        config = self._load(template_file, template_string)
+        config = self._load(template_path, template_string)
         config = self._load_includes(config)
-        #config = self._remove_excluded_roles(config, exclude_roles)
         #config = self._mutate_port_ranges(config)
         return config
--- tests/unit/test_config_loader.py
+++ tests/unit/test_config_loader.py
@@ -2,27 +2,17 @@ from unittest import TestCase
 
 from botoform.config import ConfigLoader
 
-TEMPLATE_DIR = 'tests'
-TEMPLATE_FILE = 'webapp.yaml'
+TEMPLATE_PATH = 'tests/fixtures/webapp.yaml'
 
 class TestConfigLoader(TestCase):
     def setUp(self):
-        self.template_dir    = TEMPLATE_DIR
-        self.template_file   = TEMPLATE_FILE
-        with open(TEMPLATE_DIR + '/' + TEMPLATE_FILE) as f:
+        self.template_path = TEMPLATE_PATH
+        with open(TEMPLATE_PATH) as f:
             self.template_string = f.read()
         self.context_vars = {
                  'vpc_name'  : 'webapp01',
                  'cidrblock' : '192.168.1.1/24',
              }
-
-    def test_constructor_templates_in_searchpath(self):
-        loader = ConfigLoader()
-        self.assertTrue('templates' in loader.jinja2_env.loader.searchpath)
-
-    def test_constructor_template_dir_in_searchpath(self):
-        loader = ConfigLoader(template_dir='taco')
-        self.assertTrue('taco' in loader.jinja2_env.loader.searchpath)
 
     def test_constructor_default_context_vars_is_empty_dict(self):
         loader = ConfigLoader()
@@ -30,28 +20,27 @@ class TestConfigLoader(TestCase):
         self.assertEqual(loader.context_vars, {}) 
 
     def test_constructor_context_vars_is_set(self):
-        loader = ConfigLoader(context_vars=self.context_vars)
+        loader = ConfigLoader(context_vars = self.context_vars)
         self.assertTrue(len(loader.context_vars.keys()) == 2)
         self.assertTrue('cidrblock' in loader.context_vars.keys())
 
-    def test_load_template_string_without_context(self):
-        loader = ConfigLoader(self.template_dir)
-        config = loader.load(template_string=self.template_string)
-        self.assertEqual(len(config), 9)
-        self.assertEqual(len(config['instance_roles']), 1)
-        self.assertEqual(len(config['amis']), 4)
-        self.assertEqual(len(config['security_groups']), 2)
-        self.assertEqual(len(config['security_groups']['web']), 1)
-        self.assertEqual(config['cidrblock'], '192.168.1.0/24')
-
     def test_load_template_file_without_context(self):
-        loader = ConfigLoader(self.template_dir)
-        config = loader.load(template_file = self.template_file)
-        self.assertEqual(len(config), 9)
+        loader = ConfigLoader()
+        config = loader.load(template_path = self.template_path)
+        self.assertEqual(len(config), 7)
         self.assertEqual(len(config['instance_roles']), 1)
         self.assertEqual(len(config['amis']), 4)
         self.assertEqual(len(config['security_groups']), 2)
         self.assertEqual(len(config['security_groups']['web']), 1)
-        self.assertEqual(config['cidrblock'], '192.168.1.0/24')
+
+    def test_load_template_string_without_context(self):
+        loader = ConfigLoader()
+        loader.template_dir = 'tests/fixtures'
+        config = loader.load(template_string = self.template_string)
+        self.assertEqual(len(config), 7)
+        self.assertEqual(len(config['instance_roles']), 1)
+        self.assertEqual(len(config['amis']), 4)
+        self.assertEqual(len(config['security_groups']), 2)
+        self.assertEqual(len(config['security_groups']['web']), 1)
 
 

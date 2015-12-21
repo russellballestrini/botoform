a2311473b6d22b82e7dc797d876ff6373a6eebcc
Russell Ballestrini <russell.ballestrini@mandiant.com>
2015-12-21 12:46:59 -0500

    modified:   botoform/plugins/repl.py

--- botoform/plugins/repl.py
+++ botoform/plugins/repl.py
@@ -14,11 +14,17 @@ try:
 except ImportError:
     pass
 
+BANNER = """
+You are now connected to {} ({}) in {}
+
+You now have access to the evpc object, for example: evpc.roles
+"""
+
 def REPL(args, evpc):
     """
     Open an interactive REPL (read-eval-print-loop) with access to evpc object
     """
-    msg = '\nYou now have access to the evpc object, for example: evpc.roles\n'
+    msg = BANNER.format(evpc.id, evpc.name, evpc.region_name)
     if interpreter == 'bpython':
         bpython.embed(locals_ = locals(), banner = msg)
     elif interpreter == 'ipython':

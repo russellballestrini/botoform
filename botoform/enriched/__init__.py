34526d9e9169c2a06d6f9cd7d0ccbc67ef31e6b9
Russell Ballestrini <russell.ballestrini@gmail.com>
2016-01-01 17:42:42 -0500

    Update __init__.py

--- botoform/enriched/__init__.py
+++ botoform/enriched/__init__.py
@@ -1,2 +1,11 @@
 from vpc import EnrichedVPC
+
+from vpc_endpoint import EnrichedVpcEndpoint
+
+from key_pair import EnrichedKeyPair
+
 from instance import EnrichedInstance
+
+from elb import EnrichedElb
+
+from elasticache import EnrichedElastiCache
